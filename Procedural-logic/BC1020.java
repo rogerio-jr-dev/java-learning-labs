4fad0812125e40719b6f4b272f7e8a2776bb7f00
rogerio-jr-dev <ethicalhacker.dev@gmail.com>
2026-06-04 14:32:25 -0300

    Exercise-BC1020

--- Procedural-logic/BC1020.java
+++ Procedural-logic/BC1020.java
@@ -1,7 +1,28 @@
 import java.util.Scanner; 
 public class BC1020{
     public static void main(String args[]){
-        Scanner sc =new Scanner(System.in); 
+        Scanner sc = new Scanner(System.in);
+        int age = sc.nextInt();
+
+        int years = 0;
+        int months = 0;
+        int days = 0;
+        while(age != 0) {
+            if (age > 365) {
+                age -= 365;
+                years++;
+            } else if (age >= 30) {
+                age -= 30;
+                months++;
+            } else {
+                days = age;
+                age = 0;
+            }
+        }
+
+        System.out.println(years + " ano(s)");
+        System.out.println(months + " mes(es)");
+        System.out.println(days + " dia(s)");
 
         sc.close(); 
     }
